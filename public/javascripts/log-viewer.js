a38ba3f7054772efc8e141e2cc5077e7a5c078a4
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-06-30 18:50:50 +0700

    Displaying a log file in real time

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -1,12 +1,14 @@
-const WebSocket = require("ws");
-
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
+const filePath = document.getElementById("logFilePath").value;
 
 connection.onopen = () => {
-    connection.send("Hello from the client!");
+    if (filePath) {
+        connection.send(filePath);
+    }   
 };
 
 connection.onmessage = (event) => {
+    console.log('test');
     logWindow.innerHTML = event.data;
 };
