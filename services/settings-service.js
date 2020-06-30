d5708d4f1af2bd7c2e20a3300c928841e991dc2c
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-06-30 21:51:09 +0700

    Adding settings and configuration

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,8 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const settingsFilePath = path.join(__dirname, "json", "settings.json");
+
 function getSettings() {
-  return {}
+  const settingsData = fs.readFileSync(settingsFilePath);
+  return JSON.parse(settingsData);
 }
 
 function writeSettings(newSettings) {
+  const settingsJSON = JSON.stringify(newSettings, null, 2);
+  try {
+    fs.writeFileSync(settingsFilePath, settingsJSON);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 function getDefaultDir() {
