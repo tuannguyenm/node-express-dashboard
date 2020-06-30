de2175c78413391ac1c443722dabaaf420d43373
tuannguyen <tuan.nguyenminh@nashtechglobal.com>
2020-06-30 10:41:11 +0700

    Adding the Websocket-based log viewer window

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -2,3 +2,11 @@ const WebSocket = require("ws");
 
 const connection = new WebSocket("ws://localhost:3000");
 const logWindow = document.querySelector("#log-window");
+
+connection.onopen = () => {
+    connection.send("Hello from the client!");
+};
+
+connection.onmessage = (event) => {
+    logWindow.innerHTML = event.data;
+};
